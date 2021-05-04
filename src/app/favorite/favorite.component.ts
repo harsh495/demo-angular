263d5ae69fa9f0836160aadf859e741216b7fb8e
harsh <agrawalh495@gmail.com>
2021-05-04 09:38:22 +0530

    adding changes for input aliasing

--- src/app/favorite/favorite.component.ts
+++ src/app/favorite/favorite.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit, Input} from '@angular/core';
 })
 export class FavoriteComponent implements OnInit {
 
-   @Input() isFavorite = false;
+   @Input("isSelected") isFavorite = false;
   constructor() { }
 
   ngOnInit(): void {
